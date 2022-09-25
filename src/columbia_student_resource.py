51624e0bb9b82b88ffa56b80ea253258b193f8d1
Yang Li <yal319@ucsd.edu>
2022-09-25 16:43:14 -0700

    Init Commit

---
+++ src/columbia_student_resource.py
@@ -0,0 +1,37 @@
+import pymysql
+
+import os
+
+
+class ColumbiaStudentResource:
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def _get_connection():
+
+        usr = 'root'
+        pw = 'password'
+        h = 'localhost'
+
+        conn = pymysql.connect(
+            user=usr,
+            password=pw,
+            host=h,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
+
+    @staticmethod
+    def get_by_key(key):
+
+        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
+        conn = ColumbiaStudentResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=key)
+        result = cur.fetchone()
+
+        return result
+
